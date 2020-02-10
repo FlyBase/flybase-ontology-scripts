029c7ae2ce3e5317f8e2cdf14997092b20b22c24
Clare72 <cp390@cam.ac.uk>
2020-02-10 10:11:11 +0000

    new running script to have parent fbbt term as user input with default fbbt file

---
+++ part_of_search/src/spreadsheet_from_parent_term.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from FCA_spreadsheet_tools import generate_report_from_term
+import sys
+
+"""
+Make spreadsheet using resources/fbbt-simple.json for is_a and part_of children (not just cells) of provided term.
+Use full IRI.
+Example usage:
+python3 spreadsheet_from_parent_term.py http://purl.obolibrary.org/obo/FBbt_00040005
+(= synaptic neuropil)
+"""
+
+fbbt_path = "./resources/fbbt-simple.json"
+
+generate_report_from_term(search_iri=sys.argv[1],fbbt_path=fbbt_path,xlsx_out='./anatomy_terms.xlsx',cell_only=False)

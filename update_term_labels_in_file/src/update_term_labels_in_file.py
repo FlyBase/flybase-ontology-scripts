4e7e48eff8bfdf643a0b58a8e9d752766e679420
Clare72 <cp390@cam.ac.uk>
2020-07-22 16:56:05 +0100

    no longer necessary to have a column for labels in input

--- update_term_labels_in_file/src/update_term_labels_in_file.py
+++ update_term_labels_in_file/src/update_term_labels_in_file.py
@@ -1,18 +1,20 @@
 import pandas as pd
 from vfb_connect.neo.neo4j_tools import Neo4jConnect, dict_cursor
+
 nc = Neo4jConnect('http://kb.virtualflybrain.org', 'neo4j', 'neo4j')
 
 
 def replace_labels(dataframe, id_col_name='FBbt_ID', label_col_name='FBbt_name'):
     """Updates labels from latest FBbt release (from VFB) based on IDs.
 
-    Input is a dataframe - default for ID column to be 'FBbt_ID' and label column as 'FBbt_name'."""
+    Input is a dataframe - default for ID column to be 'FBbt_ID' and label column as 'FBbt_name'.
+    Label column not required in input."""
     col_order = dataframe.columns
     FBbt_list = [str(x).replace(':', '_') for x in set(dataframe[dataframe[id_col_name].notnull()][id_col_name])]
 
     query = "MATCH (c:Class) WHERE c.short_form IN %s \
-    RETURN c.short_form AS %s, c.label AS %s"\
-    % (FBbt_list, id_col_name, label_col_name)
+    RETURN c.short_form AS %s, c.label AS %s" \
+            % (FBbt_list, id_col_name, label_col_name)
 
     q = nc.commit_list([query])
     labels = dict_cursor(q)
@@ -20,9 +22,13 @@ def replace_labels(dataframe, id_col_name='FBbt_ID', label_col_name='FBbt_name')
     labels_df = pd.DataFrame(labels)
     labels_df = labels_df.applymap(lambda x: x.replace('_', ':'))
 
-    dataframe = dataframe.drop(label_col_name, axis=1)
+    try:
+        dataframe = dataframe.drop(label_col_name, axis=1)
+    except KeyError:
+        existing_column = False
     dataframe = pd.merge(left=dataframe, right=labels_df, how='left', on=id_col_name)
-    dataframe = dataframe[col_order]
+    if existing_column:
+        dataframe = dataframe[col_order]
 
     return dataframe
 

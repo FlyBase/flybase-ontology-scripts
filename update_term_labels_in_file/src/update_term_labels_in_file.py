29b1dfa99986fcac47eeab332cf5098ba99b8bc3
Clare72 <cp390@cam.ac.uk>
2020-07-28 11:57:15 +0100

    minor updates to label script

--- update_term_labels_in_file/src/update_term_labels_in_file.py
+++ update_term_labels_in_file/src/update_term_labels_in_file.py
@@ -3,6 +3,11 @@ from vfb_connect.neo.neo4j_tools import Neo4jConnect, dict_cursor
 
 nc = Neo4jConnect('http://kb.virtualflybrain.org', 'neo4j', 'neo4j')
 
+# to run on a local file, amend details here:
+file = './file.tsv'
+id_column_name = 'FBbt_ID'
+label_column_name = 'FBbt_name'
+
 
 def replace_labels(dataframe, id_col_name='FBbt_ID', label_col_name='FBbt_name'):
     """Updates labels from latest FBbt release (from VFB) based on IDs.
@@ -10,7 +15,8 @@ def replace_labels(dataframe, id_col_name='FBbt_ID', label_col_name='FBbt_name')
     Input is a dataframe - default for ID column to be 'FBbt_ID' and label column as 'FBbt_name'.
     Label column not required in input."""
     col_order = dataframe.columns
-    FBbt_list = [str(x).replace(':', '_') for x in set(dataframe[dataframe[id_col_name].notnull()][id_col_name])]
+    FBbt_list = [str(x).replace(':', '_') for x in set(
+        dataframe[dataframe[id_col_name].notnull()][id_col_name])]
 
     query = "MATCH (c:Class) WHERE c.short_form IN %s \
     RETURN c.short_form AS %s, c.label AS %s" \
@@ -43,4 +49,6 @@ def replace_labels_in_file(filename, id_col_name='FBbt_ID', label_col_name='FBbt
 
     output_dataframe.to_csv(filename, sep='\t', index=None)
 
-# replace_labels_in_file(filename='./file.tsv')
+
+if __name__ == "__main__":
+    replace_labels_in_file(file, id_column_name, label_column_name)

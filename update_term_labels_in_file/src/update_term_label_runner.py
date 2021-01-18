3a406e6fe88406d3d05e06890cbb517b0c22fd63
Clare72 <cp390@cam.ac.uk>
2021-01-18 10:56:47 +0000

    updated folder script to ignore hidden files

--- update_term_labels_in_file/src/update_term_label_runner.py
+++ update_term_labels_in_file/src/update_term_label_runner.py
@@ -3,7 +3,7 @@ import os
 
 """Updates labels in all files in 'files' folder"""
 
-file_list = os.listdir('files')
+file_list = [f for f in os.listdir('files') if not f.startswith('.')]
 
 for f in file_list:
     update_term_labels_in_file.replace_labels_in_file(os.path.join('files', f))

459fcee01391edbc872a241c038c78e70fbd8e44
Clare72 <cp390@cam.ac.uk>
2021-03-08 13:55:03 +0000

    adding hyphens, brackets, code to find PMIDs

--- misc_checks_reports/src/references_in_FBbt.py
+++ misc_checks_reports/src/references_in_FBbt.py
@@ -10,8 +10,10 @@ if not os.path.isfile('fbbt-edit.obo'):
 fbbt = open('fbbt-edit.obo', 'r').read()
 
 # find dois
-doi_pattern = re.compile('doi:[0-9a-zA-Z./]+')
-dois = set(re.findall(doi_pattern, fbbt))
+doi_pattern = re.compile('doi:[0-9a-zA-Z./\-()]+')
+dois_raw = set(re.findall(doi_pattern, fbbt))
+dois = [d.rstrip(').') for d in dois_raw]
+dois = set(dois)
 
 doi_file = open('dois.txt', 'w')
 for d in dois:
@@ -19,7 +21,7 @@ for d in dois:
 
 
 # find urls
-url_pattern = re.compile('http[s]?://[a-zA-Z0-9./]*')
+url_pattern = re.compile('http[s]?://[a-zA-Z0-9./\-]*')
 urls = set(re.findall(url_pattern, fbbt))
 
 # filter to remove 'acceptable' urls
@@ -28,3 +30,12 @@ filtered_urls = [u for u in urls if not ('orcid' in u or 'flybase' in u or 'purl
 url_file = open('url.txt', 'w')
 for u in filtered_urls:
     url_file.write(u + '\n')
+
+
+# find PMIDs
+pmid_pattern = re.compile('PMID:[0-9]+')
+pmids = set(re.findall(pmid_pattern, fbbt))
+
+pmid_file = open('pmid.txt', 'w')
+for p in pmids:
+    pmid_file.write(p + '\n')

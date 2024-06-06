6ae8883113b8c581512f331ea35d97c30b1dc187
Clare72 <cp390@cam.ac.uk>
2024-06-06 09:45:03 +0100

    option to use local ontology file

--- update_term_labels_in_file/src/update_term_labels_in_file.py
+++ update_term_labels_in_file/src/update_term_labels_in_file.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import argparse
 from vfb_connect.neo.neo4j_tools import Neo4jConnect, dict_cursor
+from oaklib import get_adapter
 
-# connect to VFB KB
-nc = Neo4jConnect('http://kb.virtualflybrain.org', 'neo4j', 'vfb')
 
 # setup arguments that can be provided on command line
 parser = argparse.ArgumentParser()
@@ -12,6 +11,7 @@ parser.add_argument("--filename", "-f", help="Filename")
 parser.add_argument("--id_col", "-i", help="Name of column containing IDs")
 parser.add_argument("--label_col", "-l", help="Name of column for labels")
 parser.add_argument("--sep", "-s", help="Separator for lists")
+parser.add_argument("--source", "-c", help="Where to lookup labels ('VFB' or a filename)")
 args = parser.parse_args()
 
 # if not running on command line, can edit defaults here:
@@ -31,49 +31,54 @@ if args.sep:
     separator = args.sep
 else:
     separator = '|'
+if args.source:
+    source = args.source
+else:
+    source = 'VFB'
 
 
 def get_id_cols(filename=file):
-    """Returns column names from the given file if 'FBbt:' is in the column."""
+    """Returns column names from the given file if '[A-z]+:[0-9]+' is in the column."""
     mapping = pd.read_csv(filename, sep='\t', dtype=str)
 
     id_cols = []
     for col in mapping.columns:
-        if len(mapping.loc[mapping[col].str.contains("FBbt", na=False)]) > 0:
+        if len(mapping.loc[mapping[col].str.contains("[A-z]+:[0-9]+", na=False)]) > 0:
             id_cols.append(col)
 
     return id_cols
 
 
-def replace_labels(dataframe, id_col_name='FBbt_id', label_col_name='FBbt_name', sep='|'):
+def replace_labels(dataframe, id_col_name=id_column_name, label_col_name=label_column_name, sep=separator, source=source):
     """Updates labels from latest FBbt release (from VFB) based on IDs.
 
     Input is a dataframe - default for ID column to be 'FBbt_id' and label column as 'FBbt_name'.
     Label column not required in input."""
     col_order = dataframe.columns
-    dataframe['converted_ids'] = dataframe.loc[:,id_col_name].apply(
-        lambda x: (str(x).replace(':', '_')).split(sep))
-    FBbt_list = list(dataframe.loc[:,'converted_ids'])
-    flat_FBbt_list = list(set([item for sublist in FBbt_list for item in sublist]))
+    # separate ids to lists and make one flat list of unique values
+    dataframe['id_lists'] = dataframe[id_col_name].apply(lambda x: x.split(sep))
+    flat_FBbt_list = list(set(dataframe['id_lists'].explode().tolist()))
 
-    # [str(x).replace(':', '_') for x in set(
-    # dataframe[dataframe[id_col_name].notnull()][id_col_name])]
+    if source == 'VFB':  # get labels from VFB KB
+        flat_FBbt_list_converted = [item.replace(':', '_') for item in flat_FBbt_list]
 
-    query = ("MATCH (c:Class) WHERE c.short_form IN %s "
-             "RETURN c.short_form AS ID, c.label AS label"
-             % flat_FBbt_list)
+        nc = Neo4jConnect('http://kb.virtualflybrain.org', 'neo4j', 'vfb')
+        query = ("MATCH (c:Class) WHERE c.short_form IN %s "
+                 "RETURN c.short_form AS ID, c.label AS label"
+                 % flat_FBbt_list_converted)
 
-    q = nc.commit_list([query])
-    labels = dict_cursor(q)
+        q = nc.commit_list([query])
+        labels = dict_cursor(q)
+        labels_df = pd.DataFrame(labels)
+        labels_df['ID'] = labels_df['ID'].apply(lambda x: x.replace('_', ':'))
+        labels_df = labels_df.set_index('ID')
 
-    labels_df = pd.DataFrame(labels).set_index('ID')
-
-# allow label column not to be present in original spreadsheet (and add to columns to return if not there)
-    try:
-        dataframe = dataframe.drop(label_col_name, axis=1)
-    except KeyError:
-        ID_col_loc = col_order.get_loc(id_col_name)
-        col_order = col_order.insert(ID_col_loc + 1, label_col_name)
+    else:  # get labels from local file
+        ontology = get_adapter(source)
+        labels_df = pd.DataFrame({})
+        labels_df['ID'] = flat_FBbt_list
+        labels_df['label'] = labels_df['ID'].apply(lambda x: ontology.label(x))
+        labels_df = labels_df.set_index('ID')
 
     def label_lookup(ID_list):
         """Looks up labels of items of a list of IDs in labels_df and returns list of labels."""
@@ -84,10 +89,17 @@ def replace_labels(dataframe, id_col_name='FBbt_id', label_col_name='FBbt_name',
             pass
         return label_list
 
-# make column of lists of labels from column of lists of IDs
-    dataframe['label_lists'] = dataframe.loc[:,'converted_ids'].apply(
-        lambda x: label_lookup(x))
-# convert lists to strings with separator
+    # allow label column not to be present in original spreadsheet (and add to columns to return if not there)
+    try:
+        dataframe = dataframe.drop(label_col_name, axis=1)
+    except KeyError:
+        ID_col_loc = col_order.get_loc(id_col_name)
+        col_order = col_order.insert(ID_col_loc + 1, label_col_name)
+
+    # make column of lists of labels from column of lists of IDs
+    dataframe['label_lists'] = dataframe.loc[:, id_col_name].apply(
+        lambda x: label_lookup(x.split(sep)))
+    # convert lists to strings with separator
     dataframe[label_col_name] = dataframe.loc[:,'label_lists'].apply(
         lambda x: sep.join(x) if type(x) == list else x)
 
@@ -95,11 +107,11 @@ def replace_labels(dataframe, id_col_name='FBbt_id', label_col_name='FBbt_name',
     return dataframe
 
 
-def replace_labels_in_file(filename, id_col_name='FBbt_id', label_col_name='FBbt_name', sep='|'):
+def replace_labels_in_file(filename, id_col_name=id_column_name, label_col_name=label_column_name, sep=separator):
     """Updates labels from latest FBbt release (from VFB) based on IDs.
 
     Input is a file - default for ID column to be 'FBbt_id' and label column as 'FBbt_name'."""
-    input_dataframe = pd.read_csv(filename, sep='\t', dtype=str)
+    input_dataframe = pd.read_csv(filename, sep='\t', dtype=str, na_filter=False)
     output_dataframe = replace_labels(input_dataframe, id_col_name, label_col_name, sep)
 
     output_dataframe.to_csv(filename, sep='\t', index=False)

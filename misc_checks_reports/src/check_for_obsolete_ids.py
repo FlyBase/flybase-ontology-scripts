4cec0d623c064f93de3b121cb8806cc328d8ad91
Clare72 <cp390@cam.ac.uk>
2024-07-23 15:24:12 +0100

    extend check_for_obsolete_ids

--- misc_checks_reports/src/check_for_obsolete_ids.py
+++ misc_checks_reports/src/check_for_obsolete_ids.py
@@ -1,53 +1,70 @@
+import os
 import pandas as pd
-from vfb_connect.neo.neo4j_tools import Neo4jConnect, dict_cursor
-
-nc = Neo4jConnect('http://kb.virtualflybrain.org', 'neo4j', 'neo4j')
+from oaklib import get_adapter
+import update_term_labels_in_file
 
 """
-Finds obsolete IDs used within a tsv file.
+Finds obsolete IDs used within tsv files in /files
 """
 
-# to run on a local file, amend details here:
-file = './file.tsv'
-id_column_name = 'FBbt_id'
+file_list = [f for f in os.listdir('files') if not f.startswith('.')]
+check_all_columns = True
+id_column_names = ['FBbt_id']  # ignored if check_all_columns is True
+update = True
+
+ontology = get_adapter('fbbt-merged.db')
+replacement_df = pd.DataFrame(ontology.obsoletes_migration_relationships(ontology.obsoletes()), columns=['old_id', 'rel', 'new_id'])
+replacement_df = replacement_df[replacement_df['rel'] == 'IAO:0100001']
+if replacement_df['old_id'].duplicated(keep=False).any():
+    raise ValueError("Duplicate old IDs in replacement_df")
+replacement_df.set_index('old_id', inplace=True)
 
 
-def check_obsoletes_in_df(dataframe, id_col_name='FBbt_id'):
-    """Checks whether FBbt IDs in a dataframe are obsolete (in VFB).
-
+def find_obsoletes_in_df(dataframe, id_col_names=['FBbt_id'], check_all_columns=False):
+    """Checks whether FBbt IDs in a dataframe are obsolete (in replacement_df).
     Input is a dataframe - default for ID column to be 'FBbt_id'."""
-    col_order = dataframe.columns
-    FBbt_list = [str(x).replace(':', '_') for x in set(
-        dataframe[dataframe[id_col_name].notnull()][id_col_name])]
+    obsoletes = {}
+    if check_all_columns:
+        cols_to_check = dataframe.columns
+    else:
+        cols_to_check = id_col_names
+    for i in cols_to_check:
+        for c in dataframe[i].drop_duplicates():
+            if c in replacement_df.index:
+                obsoletes[i].append(c)
 
-    query = "MATCH (c:Class) WHERE c.short_form IN %s and c.is_obsolete = true\
-    RETURN c.short_form AS %s, c.label AS %s" \
-            % (FBbt_list, id_col_name, 'term_label')
-
-    q = nc.commit_list([query])
-    obsoletes = dict_cursor(q)
-
-    if len(obsoletes) > 0:
+    if len(obsoletes.keys()) > 0:
         obsoletes_df = pd.DataFrame(obsoletes)
-        obsoletes_df = obsoletes_df.applymap(lambda x: x.replace('_', ':'))
-
         print("Some obsolete terms in use:")
         print(obsoletes_df)
-        return obsoletes_df
     else:
         print("No obsolete terms in use.")
-        return False
 
 
-def check_obsoletes_in_file(filename, id_col_name='FBbt_id', label_col_name='FBbt_name'):
+def replace_obsoletes_in_df(dataframe, id_col_names=['FBbt_id'], check_all_columns=False):
+    """Checks whether FBbt IDs in a dataframe are obsolete (in replacement_df).
+    Input is a dataframe - default for ID column to be 'FBbt_id'."""
+    if check_all_columns:
+        cols_to_check = dataframe.columns
+    else:
+        cols_to_check = id_col_names
+    for i in cols_to_check:
+        dataframe[i] = dataframe[i].apply(lambda x: x if x not in replacement_df.index else replacement_df['new_id'][x])
+
+    return dataframe
+
+
+def process_obsoletes_in_file(filename, id_col_names=['FBbt_id'], check_all_columns=False, update=False):
     """Checks whether FBbt IDs in a file are obsolete (in VFB).
-
     Input is a file - default for ID column to be 'FBbt_id'."""
-    input_dataframe = pd.read_csv(filename, sep='\t')
-    output_dataframe = check_obsoletes_in_df(input_dataframe, id_col_name)
-
-    return output_dataframe
+    input_dataframe = pd.read_csv(filename, sep='\t', dtype='str')
+    if update:
+        updated_df = replace_obsoletes_in_df(input_dataframe, id_col_names, check_all_columns)
+        updated_df.to_csv(filename, sep='\t', index=False)
+    else:
+        find_obsoletes_in_df(input_dataframe, id_col_names, check_all_columns)
 
 
 if __name__ == "__main__":
-    check_obsoletes_in_file(file, id_column_name)
+    for file in file_list:
+        process_obsoletes_in_file(file, id_column_names, check_all_columns, update)

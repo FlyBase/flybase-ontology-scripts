66751aa8aa11122302955acbf58b3585b69962de
Clare72 <cp390@cam.ac.uk>
2025-06-16 15:12:36 +0100

    minor edits to label update

--- update_term_labels_in_file/src/update_term_label_runner.py
+++ update_term_labels_in_file/src/update_term_label_runner.py
@@ -6,4 +6,8 @@ import os
 file_list = [f for f in os.listdir('files') if not f.startswith('.')]
 
 for f in file_list:
-    update_term_labels_in_file.replace_labels_in_file(os.path.join('files', f))
+    update_term_labels_in_file.replace_labels_in_file(filename=os.path.join('files', f),
+                                                      id_col_name='FBbt_id',
+                                                      source='fbbt-merged.db')
+#
+#                                                      id_col_name='FBbt_id', label_col_name='FBbt_name',

6769711217be4a1e7ae678d4754f267d57a1ba19
Clare72 <cp390@cam.ac.uk>
2026-02-20 13:53:03 +0000

    update lineage labels

--- neo_label_config_yaml/lineage_terms_yaml.py
+++ neo_label_config_yaml/lineage_terms_yaml.py
@@ -13,14 +13,21 @@ lineage_df = pd.read_csv('neuroblastAnnotations.tsv', dtype='str', sep='\t')
 lineage_df = lineage_df[~lineage_df['defined_class_label'].str.contains('male|female')]
 
 yaml_lines = ['neo_node_labelling:\n']
-for nomenclature_col in ['ito_lee', 'hartenstein', 'primary', 'secondary']:
-    for i in lineage_df[lineage_df[nomenclature_col].notna()].index:
-        label = 'lineage_' + lineage_df[nomenclature_col][i]
-        FBbt = lineage_df['defined_class'][i]
-        yaml_lines.append(f'  - label: {label}\n')
-        yaml_lines.append(f'    classes:\n')
-        yaml_lines.append(f'      - {FBbt}\n')
-        yaml_lines.append(f'      - RO:0002202 some {FBbt}\n')
+nomenclature_cols = ['ito_lee', 'hartenstein', 'secondary', 'primary']
+for i, row in lineage_df.iterrows():
+    parts = []
+    for col in nomenclature_cols:
+        val = row.get(col)
+        if pd.notna(val) and val != 'nan' and not val in parts:
+            parts.append(val)
+    if not parts:
+        continue
+    label = 'lineage_' + '_'.join(parts)
+    FBbt = row['defined_class']
+    yaml_lines.append(f'  - label: {label}\n')
+    yaml_lines.append(f'    classes:\n')
+    yaml_lines.append(f'      - {FBbt}\n')
+    yaml_lines.append(f'      - RO:0002202 some {FBbt}\n')
 
 yaml_lines.append(f'  - label: primary_neuron\n')
 yaml_lines.append(f'    classes:\n')
